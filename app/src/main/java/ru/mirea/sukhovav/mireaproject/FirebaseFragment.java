fc1e4d45a98acb7a18be7f2c650b18bf06116fc1
anton <anton.sukhov228@gmail.com>
2023-11-26 15:59:52 +0300

    Please enter the commit message for your changes. Lines starting

--- app/src/main/java/ru/mirea/sukhovav/mireaproject/FirebaseFragment.java
+++ app/src/main/java/ru/mirea/sukhovav/mireaproject/FirebaseFragment.java
@@ -1,17 +1,29 @@
 package ru.mirea.sukhovav.mireaproject;
 
+import static androidx.constraintlayout.widget.ConstraintLayoutStates.TAG;
+
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.concurrent.Executor;
+
 import ru.mirea.sukhovav.mireaproject.databinding.ActivityMainBinding;
+
 
 public class FirebaseFragment extends Fragment {
 
@@ -19,6 +31,7 @@ public class FirebaseFragment extends Fragment {
     private String mParam1;
     private String mParam2;
     private FirebaseAuth mAuth;
+    private FirebaseApp app;
     private ActivityMainBinding binding;
 
 
@@ -26,15 +39,82 @@ public class FirebaseFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mAuth = FirebaseAuth.getInstance();
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        Log.d(TAG, FirebaseApp.getInstance().getOptions().getProjectId());
+        // mireaproject2-e3f9b
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_firebase, container, false);
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if (user != null) {
+
+
+        } else {
+
+        }
+    }
+
+    public void create(String email, String password){
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener((Executor) this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(currentUser);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(getActivity(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(currentUser);
+                        }
+                    }
+                });
+
+    }
+    private void signIn(String email, String password) {
+        Log.d(TAG, "signIn:" + email);
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener((Executor) this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                        } else {
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(getActivity(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                        //if (!task.isSuccessful()) {
+                        //    binding.statusTextView.setText(R.string.auth_failed);
+                        //}
+                    }
+                });
+    }
+
+    private void signOut() {
+        mAuth.signOut();
+        //updateUI(null);
+    }
+
+    
 
 
 
